86c66c7409b7e1556449b0ccf620740b0bbed79d
AI&Multimeida&BigData @OUC <143200561+huanglab-research@users.noreply.github.com>
2024-02-20 15:17:38 +0800

    Update DCT_mask.py

--- function/DCT_mask.py
+++ function/DCT_mask.py
@@ -10,19 +10,12 @@ def dct_frequence(image):
     image = np.transpose(image, (1, 2, 0))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     dct = cv2.dct(np.float32(image))
-
     threshold = 0.01  
     mask = np.ones_like(dct)
     mask[dct < threshold * np.max(dct)] = 0
-
     idct = cv2.idct(mask * dct)
-
-
     cv2.imwrite("/path/func/mask.png", idct.astype(np.uint8))
-
     mean_value = np.mean(idct)
-
-
     binary_spectrum = np.where(idct > 1.5*mean_value, 0, 255)
     #print(binary_spectrum)
     cv2.imwrite("/path/func/mask_01.png", binary_spectrum)
